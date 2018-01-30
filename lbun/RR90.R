d84bcc9c97ed4d1360542f12cbd031b7bad12138
Linh Bun <bunlinh@hotmail.com>
2018-01-30 11:34:03 -0800

    Adding Copy of RR90all.R
    - making a copy of RR90all.R (under CaseStudies directory of revisit package)
    - editing my RR90.R and R Markdown files

--- lbun/RR90.R
+++ lbun/RR90.R
@@ -1,3 +1,10 @@
+# finding the path for "revisit" package in R
+system.file(package = "revisit")
+# getting the list of files
+dir()
+# getting data directory
+system.file("data", package = "revisit")
+
 
 
 # Reinhart and Rogoff Replication
